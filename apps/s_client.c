854eb9c88da8b742c1d77a11058fcd0d4036c0da
Peter Mosmans <support@go-forward.net>
2014-08-11 12:16:02 +0200

    Support xmpp double quote support
    This issue was earlier raised in 2010 (see
    https://bugzilla.redhat.com/show_bug.cgi?id=608239)
    and 2012 (see RT #2860)

--- apps/s_client.c
+++ apps/s_client.c
@@ -735,7 +735,7 @@ static char *jpake_secret = NULL;
                         {
                         if (--argc < 1) goto bad;
                         http_proxy_str=*(++argv);
-                        }                
+                        }
 		else if	(strcmp(*argv,"-verify") == 0)
 			{
 			verify=SSL_VERIFY_PEER;
@@ -1730,7 +1730,7 @@ SSL_set_tlsext_status_ids(con, ids);
 		    "xmlns='jabber:client' to='%s' version='1.0'>", servername?servername:host);
 		seen = BIO_read(sbio,mbuf,BUFSIZZ);
 		mbuf[seen] = 0;
-		while (!strstr(mbuf, "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'"))
+		while (!strstr(mbuf, "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'") && !strstr(mbuf, "<starttls xmlns=\"urn:ietf:params:xml:ns:xmpp-tls\""))
 			{
 			if (strstr(mbuf, "/stream:features>"))
 				goto shut;
