bdc9a3f495c4af978da124a0eec03a7ece1d6099
Hubert Kario <hkario@redhat.com>
2015-01-25 20:30:44 +0100

    implement -no_tlsext
    Some servers implement TLS1.0 but are intolerant to any and all
    extensions sent by clients. Implement option to disable sending
    of the extensions completely.
    
    # Conflicts:
    #	ssl/t1_lib.c

--- ssl/ssl.h
+++ ssl/ssl.h
@@ -595,6 +595,10 @@ struct ssl_session_st {
 /* Refers to ancient SSLREF and SSLv2, retained for compatibility */
 # define SSL_OP_SSLREF2_REUSE_CERT_TYPE_BUG              0x0
 
+/* Disables sending any TLS extensions in client hello, even if required
+ * by used protocol version, ciphers or other options */
+# define SSL_OP_NO_TLSEXT                                0x00000400L
+
 /*
  * Disable SSL 3.0/TLS 1.0 CBC vulnerability workaround that was added in
  * OpenSSL 0.9.6d.  Usually (depending on the application protocol) the
